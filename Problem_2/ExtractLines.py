de68d6fa63057fedfd4ea1df5a1dd0bdf91c9e7b
brandonfranz13 <brandonfranz13@gmail.com>
2020-10-18 20:39:07 -0500

    wip

--- Problem_2/ExtractLines.py
+++ Problem_2/ExtractLines.py
@@ -212,9 +212,9 @@ def MergeColinearNeigbors(theta, rho, alpha, r, pointIdx, params):
           merge. If it can be split, do not merge.
     '''
     ########## Code starts here ##########
-    alphaOut = [0]
-    rOut = [0]
-    pointIdxOut = [0, 0]
+    alphaOut = np.zeros(0)
+    rOut = np.zeros(0)
+    pointIdxOut = np.zeros((0,2))
     N_lines = len(pointIdx[:,0])
     for i in range(1, N_lines):
         startIdx = pointIdx[i-1, 0]
