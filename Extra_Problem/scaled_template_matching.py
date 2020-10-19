cb5a447ff92583c1698fde70ab904391ccbb126e
brandonfranz13 <brandonfranz13@gmail.com>
2020-10-19 15:19:42 -0500

    wip

--- Extra_Problem/scaled_template_matching.py
+++ Extra_Problem/scaled_template_matching.py
@@ -3,7 +3,7 @@
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
-from Problem_4.template_matching import *
+import template_matching as tm
 
 
 def template_match(template, image,
@@ -31,7 +31,7 @@ def template_match(template, image,
         pyramid[i+1] = cv2.pyrDown(pyramid[i])
     
     for img in pyramid:
-        matches = np.vstack(matches, Problem_4.template_matching.template_match(template, img, detection_threshold))
+        matches = np.vstack(matches, tm.template_match(template, img, detection_threshold))
     
     return matches
     ########## Code ends here ##########
