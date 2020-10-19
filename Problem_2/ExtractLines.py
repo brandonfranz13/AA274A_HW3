a0357c2980d6daf9b1d9b9606c2641e456b7aaf9
brandonfranz13 <brandonfranz13@gmail.com>
2020-10-18 19:07:50 -0500

    wip

--- Problem_2/ExtractLines.py
+++ Problem_2/ExtractLines.py
@@ -128,7 +128,7 @@ def SplitLinesRecursive(theta, rho, startIdx, endIdx, params):
     alpha1, r1, i1 = SplitLinesRecursive(theta, rho, startIdx, startIdx + s, params)
     alpha2, r2, i2 = SplitLinesRecursive(theta, rho, startIdx + s, endIdx, params)
     alpha = np.append(alpha, alpha1, alpha2])
-    r = np.array(r, r1, r2])
+    r = np.array(r, r1, r2)
     idx = np.vstack([i1, i2])
     ########## Code ends here ##########
     return alpha, r, idx
