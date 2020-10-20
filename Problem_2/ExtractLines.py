1ef84f9854f99b6b8e09a2b578ec7c0a62cacfd8
brandonfranz13 <brandonfranz13@gmail.com>
2020-10-20 01:12:30 -0500

    wip

--- Problem_2/ExtractLines.py
+++ Problem_2/ExtractLines.py
@@ -158,10 +158,10 @@ def FindSplit(theta, rho, alpha, r, params): ##FAILED##
         distance = np.abs((rho[splitIdx]*np.cos(theta[splitIdx] - alpha) - r))
         if distance >= params['LINE_POINT_DIST_THRESHOLD']:
             possibleSplits = np.append(possibleSplits, (splitIdx, distance))
-    maxIdx = np.argmax(possibleSplits[:,1])
+    maxIdx = np.argmax(possibleSplits[:][1])
     
     if possibleSplits.size != 0:
-        return possibleSplits[maxIdx[0], 0]
+        return possibleSplits[maxIdx[0]][0]
     else:
         splitIdx = -1
     
