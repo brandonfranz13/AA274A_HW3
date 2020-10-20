5646dc4e991f7f5c0b3f9d02657b6eb1a09c489a
brandonfranz13 <brandonfranz13@gmail.com>
2020-10-20 00:36:00 -0500

    wip

--- Problem_2/ExtractLines.py
+++ Problem_2/ExtractLines.py
@@ -156,7 +156,7 @@ def FindSplit(theta, rho, alpha, r, params): ##FAILED##
     print(theta, rho, alpha, r)
     for splitIdx in range(params['MIN_POINTS_PER_SEGMENT'], len(theta)-params['MIN_POINTS_PER_SEGMENT']):
         if np.abs((rho[splitIdx]*np.cos(theta[splitIdx] - alpha) - r)) > params['LINE_POINT_DIST_THRESHOLD']:
-            return splitIdx
+            return splitIdx+1
     splitIdx = -1
     
     ########## Code ends here ##########
